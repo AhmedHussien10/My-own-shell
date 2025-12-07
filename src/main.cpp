ed5eb6db63dad2bae5ec930f59bd94a3edaf9577
Ahmed Hussien <ahussien311@gmail.com>
2025-12-07 19:11:16 +0200

    a

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,6 @@
 #include <string>
 #include <cstdlib>
 #include <filesystem>
-
-#ifdef _WIN32
-#include <Windows.h> // Only on Windows
-#else
-#include <unistd.h> // For POSIX systems (Linux/macOS)
-#include <sys/wait.h> // For wait() function on Unix systems
-#endif
-
 using namespace std;
 namespace fs = std::filesystem;
 
@@ -35,12 +27,14 @@ void search_executable_in_path(const string& command) {
         return;
     }
 
-    vector<string> path_dirs = split_path(path_env, (sizeof(char) == 1 && *path_env == '/') ? ':' : ';'); // ':' for Unix and ';' for Windows
+    vector<string> path_dirs = split_path(path_env, ':');
 
     for (const auto& dir : path_dirs) {
         fs::path full_path = fs::path(dir) / command;
 
-        if (!fs::exists(dir)) continue;
+        if (!fs::exists(dir)) {
+            continue;
+        }
 
         if (fs::exists(full_path) && fs::is_regular_file(full_path)) {
             if ((fs::status(full_path).permissions() & fs::perms::owner_exec) != fs::perms::none) {
@@ -53,42 +47,6 @@ void search_executable_in_path(const string& command) {
     cout << command << ": not found\n";
 }
 
-void run_external_program(const string& command, const vector<string>& args) {
-    vector<const char*> c_args;
-    c_args.push_back(command.c_str());
-
-    // Convert each argument to const char* and add it to the vector
-    for (const string& arg : args) {
-        c_args.push_back(arg.c_str());
-    }
-
-    c_args.push_back(nullptr); // Null-terminate the array of arguments
-
-#ifdef _WIN32
-    // Windows: use system to run the command
-    int result = system(command.c_str());
-#else
-    // Unix-like: use fork and execvp
-    pid_t pid = fork();
-
-    if (pid == 0) {
-        execvp(command.c_str(), const_cast<char* const*>(c_args.data()));
-        exit(0); // execvp will replace the current process, so this is just a fallback.
-    } else if (pid > 0) {
-        wait(NULL); // Parent process waits for the child to finish
-    } else {
-        cerr << "Fork failed!" << endl;
-    }
-#endif
-
-    // If execvp or system fails
-#ifdef _WIN32
-    if (result != 0) {
-        cerr << "Error: Unable to execute " << command << endl;
-    }
-#endif
-}
-
 int main() {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
@@ -96,6 +54,7 @@ int main() {
     string command;
 
     cout << "$ ";
+
     while (getline(cin, command)) {
         if (command.empty()) {
             cout << "$ ";
@@ -106,42 +65,32 @@ int main() {
             break;
         }
 
-        if (command.find("echo") == 0) {
+        else if (command.find("echo") == 0) {
             size_t space = command.find(' ');
+
             if (space != string::npos) {
                 string toprint = command.substr(space + 1);
                 cout << toprint;
             }
+
             cout << "\n";
-        } else if (command.find("type") == 0) {
+        }
+
+        else if (command.find("type") == 0) {
             size_t space = command.find(' ');
             if (space != string::npos) {
                 string func = command.substr(space + 1);
+
                 if (func == "echo" || func == "exit" || func == "type") {
                     cout << func << " is a shell builtin\n";
                 } else {
                     search_executable_in_path(func);
                 }
             }
-        } else {
-            size_t space = command.find(' ');
-            string cmd_name = command.substr(0, space);
+        }
 
-            vector<string> args;
-            if (space != string::npos) {
-                stringstream ss(command.substr(space + 1));
-                string arg;
-                while (getline(ss, arg, ' ')) {
-                    args.push_back(arg);
-                }
-            }
-
-            if (cmd_name == "exit") {
-                break;
-            }
-
-            search_executable_in_path(cmd_name);
-            run_external_program(cmd_name, args);
+        else {
+            cout << command << ": command not found" << std::endl;
         }
 
         cout << "$ ";
